297fa8bc80a0d562fce9322f0fd127f4b6a8ae32
mckayclawson <mckay.clawson@gmail.com>
2014-09-28 22:54:56 -0400

    ugg

--- FTP.cs
+++ FTP.cs
@@ -189,9 +189,10 @@ namespace FTP
                                 int portHi = ((port >> 8) & 0xff);
                                 int portLo = ((port >> 0) & 0xff);
                                 String[] ipBlock = Regex.Split(ip,"\\.");
-                                TcpClient clientSocket = portListener.AcceptTcpClient();
+                                
                                 Console.WriteLine(ipBlock[0] + "," + ipBlock[1] + "," + ipBlock[2] + "," + ipBlock[3] + "," + portHi + "," + portLo);
                                 sendCommand(writer,"PORT " + ipBlock[0]+","+ipBlock[1]+","+ipBlock[2]+","+ipBlock[3]+","+portHi+","+portLo);
+                                TcpClient clientSocket = portListener.AcceptTcpClient();
                                 StreamReader pa = new StreamReader(clientSocket.GetStream());
                                 Console.Write(getResponse(pa));
                                 Console.Write(getResponse(reader));
